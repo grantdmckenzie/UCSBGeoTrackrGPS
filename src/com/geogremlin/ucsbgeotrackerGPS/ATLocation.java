647edacb2ae4678c0104831c514a72ab10578f7b
Grant McKenzie <grantdmckenzie@gmail.com>
2011-11-20 23:34:44 +0100

    fixed get last known fix issue

--- src/com/geogremlin/ucsbgeotrackerGPS/ATLocation.java
+++ src/com/geogremlin/ucsbgeotrackerGPS/ATLocation.java
@@ -80,8 +80,8 @@ public class ATLocation extends Service {
 		// String Text = "Last Latitude = " + currentLocation.getLatitude() + "\nLast Longitude = " + currentLocation.getLongitude();
 		// Toast.makeText(this, Text, Toast.LENGTH_SHORT).show();
 		
-		Timestamp ts = new Timestamp(Calendar.getInstance().getTime().getTime());
-		storeData(""+currentLocation.getLatitude(), ""+currentLocation.getLongitude(), ""+ts);
+		// Timestamp ts = new Timestamp(Calendar.getInstance().getTime().getTime());
+		// storeData(""+currentLocation.getLatitude(), ""+currentLocation.getLongitude(), ""+ts);
 		
 	    locationListener = new MyLocationListener();
 	}
@@ -189,5 +189,6 @@ public class ATLocation extends Service {
 	    
 	    return HTTPHelper.request(response);
 	}
+	
 }
 
